21f1ae19c9417853c7114f510768074cda7c967d
mdow814 <mdow814@gmail.com>
2020-12-03 08:24:19 -0500

    added plasma

--- configuration.nix
+++ configuration.nix
@@ -43,9 +43,17 @@
   services.xserver.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.layout = "us";
   services.bamf.enable = true;
   services.openssh.enable = true; 
+  services.picom = {
+   enable = true;
+   fade = true;
+   inactiveOpacity = 0.9;
+   shadow = true;
+   fadeDelta = 4;
+  };
 
   sound.enable = true;
   hardware.pulseaudio.enable = true;
@@ -70,7 +78,7 @@
     google-chrome
     xfce.xfce4-whiskermenu-plugin 
     arc-theme
-    steam 
+    (steam.override { extraPkgs = pkgs: [ mono gtk3 gtk3-x11 zlib ];}) 
     virt-manager
     plank
     vscode
@@ -79,7 +87,8 @@
     gnome3.file-roller
     neofetch
     gnome3.adwaita-icon-theme
-    hicolor-icon-theme
+    dolphin
+    konsole
   ];
   
   system.stateVersion = "unstable";
