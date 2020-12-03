1c0cbe6ad1ca290b86008aff323d7052fe211109
Michael Downey <44315707+mdow814@users.noreply.github.com>
2020-12-02 20:11:25 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -9,35 +9,51 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  networking.hostName = "ni14ae"; 
+  
   time.timeZone = "EST5EDT";
+  
+  networking.hostName = "ni14ae"; 
   networking.useDHCP = false;
   networking.interfaces.enp39s0.useDHCP = true;
   networking.interfaces.enp49s0f3u1u3.useDHCP = true;
   networking.hostId = "854260d3";
+  
   i18n.defaultLocale = "en_US.UTF-8";
+  
   services.xserver.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.layout = "us";
+  
   sound.enable = true;
   hardware.pulseaudio.enable = true;
+  
   users.users.mdow = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
+  
+  # needed for steam/chrome
   nixpkgs.config.allowUnfree = true;
   hardware.opengl.driSupport32Bit = true;
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   hardware.pulseaudio.support32Bit = true;
+  
   environment.systemPackages = with pkgs; [
-    wget vim htop git
-    firefox google-chrome
+    wget 
+    vim 
+    htop 
+    git
+    firefox 
+    google-chrome
     xfce.xfce4-whiskermenu-plugin 
     arc-theme
-    steam virt-manager 
+    steam 
+    virt-manager 
   ];
+  
   services.openssh.enable = true;
+  
   system.stateVersion = "unstable"; # Did you read the comment?
 
 }
